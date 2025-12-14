ea1bddedfbf7023f074a31223fa8a459b59c5183
wilsonz0 <wilsonzhang1313@gmail.com>
2025-12-14 01:28:30 -0500

    add basic health system functionality to the player

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem; // New Input System
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2DMovement))]
+[RequireComponent(typeof(HealthController))]
+
 public class PlayerController : MonoBehaviour
 {
     public PlayerControls playerControls;
     private InputAction move;
+    private InputAction fire;
     
     Rigidbody2DMovement movementComponent;
     Vector2 moveDirection;
 
+    HealthController healthController;
+
     private void Awake()
     {
         movementComponent = GetComponent<Rigidbody2DMovement>();
+        healthController = GetComponent<HealthController>();
         playerControls = new PlayerControls();
     }
 
@@ -20,11 +26,16 @@ public class PlayerController : MonoBehaviour
     {
         move = playerControls.Player.Move;
         move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += FireTriggered;
     }
 
     private void OnDisable()
     {
         move.Disable();
+        fire.Disable();
     }
 
     public void Update()
@@ -35,5 +46,10 @@ public class PlayerController : MonoBehaviour
     public void FixedUpdate()
     {
         movementComponent.Move(moveDirection);
+    }
+
+    private void FireTriggered(InputAction.CallbackContext context)
+    {
+        healthController.TakeDamage(1);     // TESTING: every time you left click, health decreases by 1
     }
 }
