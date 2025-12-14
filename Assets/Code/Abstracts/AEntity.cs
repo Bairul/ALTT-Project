c43b163d1ce44f65d69f040d26f978dc29151a6f
wilsonz0 <wilsonzhang1313@gmail.com>
2025-12-14 12:32:06 -0500

    remove abstract folder to prepare for new system

--- Assets/Code/Abstracts/AEntity.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class AEntity : MonoBehaviour {
-    private string Name { get; set; }
-    private int Hp { get; set; }
-    private int Str { get; set; }
-    private int Speed { get; set; }
-    public abstract void Move();
-}
